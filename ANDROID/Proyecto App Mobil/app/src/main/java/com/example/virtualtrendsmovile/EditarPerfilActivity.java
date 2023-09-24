d1a9aa523f5e5f1ed8900556058ce96c2c117e9a
Julian <julian.meneses11@gmail.com>
2023-09-24 17:06:39 -0300

    Pantallas Inicio y Login

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/EditarPerfilActivity.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/EditarPerfilActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EditarPerfil extends AppCompatActivity {
+public class EditarPerfilActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/HomeActivity.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/HomeActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_home);
     }
+
 }
