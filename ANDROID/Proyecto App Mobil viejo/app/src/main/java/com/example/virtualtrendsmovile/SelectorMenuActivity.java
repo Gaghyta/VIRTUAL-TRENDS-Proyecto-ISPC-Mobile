33ca346895dab87b647f34dd8690738039a441d6
Agostina <ago.95fiore@hotmail.com>
2023-09-25 23:00:16 -0300

    AF subo barra de navegacion con sus iconos y fuente de letra

---
+++ ANDROID/Proyecto App Mobil viejo/app/src/main/java/com/example/virtualtrendsmovile/SelectorMenuActivity.java
@@ -0,0 +1,15 @@
+package com.example.virtualtrendsmovile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SelectorMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selector_menu);
+    }
+
+}
