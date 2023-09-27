e4d9a873a4b849255ef4a3e83cc93383a6759af3
Julian <julian.meneses11@gmail.com>
2023-09-27 19:26:34 -0300

    menu y boton confirmar en login

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/HomeActivity.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.example.virtualtrendsmovile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,4 +16,34 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+   public boolean onOptionsItemSelected (@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_home) {
+            launchHomeActivity();
+            return true;
+        } else if (id == R.id.menu_editar_perfil) {
+            launchEditarPerfilActivity();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    public void launchHomeActivity() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchEditarPerfilActivity() {
+        Intent intent = new Intent(this, EditarPerfilActivity.class);
+        startActivity(intent);
+    }
 }
