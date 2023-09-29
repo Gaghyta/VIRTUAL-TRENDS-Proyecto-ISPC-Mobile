a934d7b959a390cd0c2302267a63cacd1aea10ae
Carla Antonini <carla.antonini@hotmail.com>
2023-09-29 06:34:23 -0300

    CA subo union de selector con pantalla nuestros serviciops

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/SelectorActivity.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/SelectorActivity.java
@@ -2,7 +2,12 @@ package com.example.virtualtrendsmovile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+
 
 public class SelectorActivity extends AppCompatActivity {
 
@@ -10,6 +15,22 @@ public class SelectorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selector);
+
+
+        Button botonIrANuestroServicio = findViewById(R.id.botonIrANuestroServicio);
+
+        botonIrANuestroServicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Iniciar la actividad NuestroServicio cuando se haga clic en el botón.
+                Intent intent = new Intent(SelectorActivity.this, NuestroServicio.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+
 
 }
