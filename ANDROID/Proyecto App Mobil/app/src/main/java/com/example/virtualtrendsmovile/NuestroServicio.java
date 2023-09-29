a500297b9769eccac44ffbdec2639f37095469fb
Carla Antonini <carla.antonini@hotmail.com>
2023-09-29 07:26:38 -0300

    CA modificaciones metodos clase para orientación vertical u horizontal

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/NuestroServicio.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/NuestroServicio.java
@@ -1,11 +1,13 @@
 package com.example.virtualtrendsmovile;
 
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class NuestroServicio extends AppCompatActivity {
 
@@ -23,8 +25,31 @@ public class NuestroServicio extends AppCompatActivity {
         videoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/raw"+R.raw.body_scanner_3d_1)); //identificador del video R.raw va a la carpeta
 
         videoView.start();
+
     }
 
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Verifica la orientación del dispositivo
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // Orientación horizontal
+            // Aquí puedes realizar ajustes específicos para la orientación horizontal
+            videoView.setLayoutParams(new ConstraintLayout.LayoutParams(
+                    ConstraintLayout.LayoutParams.MATCH_PARENT,
+                    ConstraintLayout.LayoutParams.MATCH_PARENT
+            ));
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            // Orientación vertical
+            // Aquí puedes realizar ajustes específicos para la orientación vertical
+            videoView.setLayoutParams(new ConstraintLayout.LayoutParams(
+                    ConstraintLayout.LayoutParams.MATCH_PARENT,
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT
+            ));
+        }
+    }
 
     /*private Uri getMedia(String mediaName){
         return Uri.parse("android.resource://"+getPackageName()+"/raw"+mediaName);
