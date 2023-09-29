c7f3d43d463ae85ecf50ff78ad6d82aeb2672a8e
Agostina <ago.95fiore@hotmail.com>
2023-09-28 21:21:54 -0300

    AF subo logica de navegacion

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/TurneroActivity.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/TurneroActivity.java
@@ -10,5 +10,7 @@ public class TurneroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_turnero);
+
+
     }
 }
