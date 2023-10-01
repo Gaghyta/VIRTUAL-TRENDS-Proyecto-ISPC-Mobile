4e6253b54d0d0c84dacb9295392b81d5d2b1318c
yoniea <yea52735@gmail.com>
2023-10-01 00:43:11 -0300

    creación pantalla turnero

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/TurneroActivity.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/TurneroActivity.java
@@ -1,8 +1,14 @@
 package com.example.virtualtrendsmovile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class TurneroActivity extends AppCompatActivity {
 
@@ -10,7 +16,26 @@ public class TurneroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_turnero);
+        BottomNavigationView nav = findViewById(R.id.btnNavSelector);
+        nav.setSelected(true);
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id==R.id.back){
 
+                } else if (id==R.id.info) {
+                    startActivity(new Intent(getApplicationContext(), ContactoActivity.class));
+                }else if (id== R.id.map){
+
+                } else if (id==R.id.turn) {
+                    startActivity(new Intent(getApplicationContext(), TurnosActivity.class));
+                } else if (id==R.id.logout) {
+
+                }
+                return false;
+            }
+        });
 
     }
 }
