76adaa2b77b285fc3382c35a1f879a8334fe8994
Julian <julian.meneses11@gmail.com>
2023-10-01 17:43:44 -0300

    Barra Navegacion y boton aceptar en login, inicio y register activities

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/RegisterActivity.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.virtualtrendsmovile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void launchSelectorActivity(View view) {
+        Intent intent = new Intent(this, SelectorActivity.class);
+        startActivity(intent);
     }
 }
