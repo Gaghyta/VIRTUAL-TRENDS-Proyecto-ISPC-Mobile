43fb6dc18a75bcf48bb3c0a7b68e0ca5cb33a375
Carla Antonini <carla.antonini@hotmail.com>
2023-10-01 03:10:27 -0300

    CA subo parte de navegacion para DondeEstamos

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/DondeEstamosActivity.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/DondeEstamosActivity.java
@@ -1,7 +1,14 @@
 package com.example.virtualtrendsmovile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class DondeEstamosActivity extends AppCompatActivity {
 
@@ -9,6 +16,27 @@ public class DondeEstamosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donde_estamos);
+
+        BottomNavigationView nav = findViewById(R.id.btnNavSelector);
+        nav.setSelected(true);
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id==R.id.back){
+                    startActivity(new Intent(getApplicationContext(), TurnosActivity.class));
+                } else if (id==R.id.info) {
+                    startActivity(new Intent(getApplicationContext(), ContactoActivity.class));
+                }else if (id== R.id.map){
+                    startActivity(new Intent(getApplicationContext(), DondeEstamosActivity.class));
+                } else if (id==R.id.turn) {
+                    startActivity(new Intent(getApplicationContext(), TurneroActivity.class));
+                } else if (id==R.id.logout) {
+
+                }
+                return false;
+            }
+        });
     }
 
 }
