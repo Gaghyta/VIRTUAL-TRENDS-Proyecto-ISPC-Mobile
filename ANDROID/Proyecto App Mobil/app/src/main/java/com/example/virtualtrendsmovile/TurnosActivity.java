ca6c991ea4ab3975e28ddf94d9c045ac3372a30d
Carla Antonini <carla.antonini@hotmail.com>
2023-10-01 03:44:27 -0300

    Subo en pantalla TURNOS ACTIVITY ACCION HACIA DONDE ESTAMOS (map)

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/TurnosActivity.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/TurnosActivity.java
@@ -30,7 +30,7 @@ public class TurnosActivity extends AppCompatActivity {
                 } else if (id==R.id.info) {
                     startActivity(new Intent(getApplicationContext(), ContactoActivity.class));
                 }else if (id== R.id.map){
-
+                    startActivity(new Intent(getApplicationContext(), DondeEstamosActivity.class));
                 } else if (id==R.id.turn) {
                     startActivity(new Intent(getApplicationContext(), NuestroServicio.class));
                 } else if (id==R.id.logout) {
