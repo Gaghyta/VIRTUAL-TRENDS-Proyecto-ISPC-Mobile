3280c89e26e7da7f98704d2d3c3546bd57eabb1d
Carla Antonini <carla.antonini@hotmail.com>
2023-10-01 22:30:46 -0300

    SUBO CORRECCION ContactoActivity

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/ContactoActivity.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/ContactoActivity.java
@@ -2,8 +2,15 @@ package com.example.virtualtrendsmovile;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.snackbar.Snackbar;
 
 public class ContactoActivity extends AppCompatActivity {
@@ -12,11 +19,36 @@ public class ContactoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacto);
+
+        BottomNavigationView nav = findViewById(R.id.btnNavSelector);
+        nav.setSelected(true);
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id==R.id.back){
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                } else if (id==R.id.info) {
+                    startActivity(new Intent(getApplicationContext(), ContactoActivity.class));
+                }else if (id== R.id.map){
+                    startActivity(new Intent(getApplicationContext(), DondeEstamosActivity.class));
+                } else if (id==R.id.turn) {
+                    startActivity(new Intent(getApplicationContext(), NuestroServicio.class));
+                } else if (id==R.id.logout) {
+                    startActivity(new Intent(getApplicationContext(), InicioActivity.class));
+                }
+                return false;
+            }
+        });
+
+
+
+
     }
 
     public void enviar(View view) {
 
-        Snackbar.make(view, "Alert temporal", Snackbar.LENGTH_SHORT).show();
+        Snackbar.make(view, "Mensaje recibido", Snackbar.LENGTH_SHORT).show();
 
         view.postDelayed(new Runnable() {
             @Override
