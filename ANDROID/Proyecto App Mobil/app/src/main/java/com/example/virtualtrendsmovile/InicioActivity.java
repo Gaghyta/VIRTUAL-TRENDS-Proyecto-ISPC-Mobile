9d804505b04936b4e51e1f4211eece583443310b
Julian <julian.meneses11@gmail.com>
2023-10-16 19:21:25 -0300

    menu hamburguesa pantalla inicio

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/InicioActivity.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/InicioActivity.java
@@ -1,7 +1,10 @@
 package com.example.virtualtrendsmovile;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,17 +14,52 @@ import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 public class InicioActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    BottomNavigationView bottomNav;
+    ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
-        BottomNavigationView nav = findViewById(R.id.btnNavSelector);
-        nav.setSelectedItemId(R.id.logout);
-        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        drawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if(id==R.id.home){
+                    startActivity(new Intent(getApplicationContext(), InicioActivity.class));
+                }  else if (id==R.id.profile) {
+                    startActivity(new Intent(getApplicationContext(), EditarPerfilActivity.class));
+                }  else if (id==R.id.calendar) {
+                    startActivity(new Intent(getApplicationContext(), TurnosActivity.class));
+                }
+                return false;
+            }
+        });
+
+        bottomNav = findViewById(R.id.btnNavSelector);
+        bottomNav.setSelectedItemId(R.id.logout);
+        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
@@ -49,5 +87,16 @@ public class InicioActivity extends AppCompatActivity {
     public void launchRegisterActivity(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 }
