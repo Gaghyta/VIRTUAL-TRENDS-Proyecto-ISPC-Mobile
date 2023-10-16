a93763d185e6829caf8e7055359659db541e5ff1
Carla Antonini <carla.antonini@hotmail.com>
2023-10-16 04:15:05 -0300

    CA Creación de clase Turno para tener un objeto turno con los datos del mismo

---
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/modelos/Turno.java
@@ -0,0 +1,10 @@
+package com.example.virtualtrendsmovile.modelos;
+
+public class Turno {
+    private String idTurno;
+    private String fechaTurno;
+    private String franjaHoraria;
+    private String numeroDeComprobante;
+    private String email;
+    private String password;
+}
