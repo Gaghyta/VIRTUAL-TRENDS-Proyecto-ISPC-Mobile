79ebe76ec4379c77d410f9db6c4533c36ecda280
Agostina <ago.95fiore@hotmail.com>
2023-10-20 17:51:20 -0300

    AF subo pantalla de Get Started para iniciar como administrador o cliente

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/actividades/admin/Cuenta_admin.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/actividades/admin/Cuenta_admin.java
@@ -1,4 +1,4 @@
-package com.example.virtualtrendsmovile.modelos;
+package com.example.virtualtrendsmovile.actividades.admin;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
