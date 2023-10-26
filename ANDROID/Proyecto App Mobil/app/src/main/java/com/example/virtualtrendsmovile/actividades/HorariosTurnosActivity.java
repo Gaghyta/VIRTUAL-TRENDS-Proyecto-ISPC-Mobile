3a3516201c323aa839d1b2cde96e13698967c3eb
Cintia Osorio Duarte <cintia.o.duarte@gmail.com>
2023-10-26 20:41:56 -0300

    CO Importo boton Android

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/actividades/HorariosTurnosActivity.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/actividades/HorariosTurnosActivity.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.Button;
 import com.example.virtualtrendsmovile.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
