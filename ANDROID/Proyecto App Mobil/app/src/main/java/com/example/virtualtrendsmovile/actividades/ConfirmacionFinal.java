d138e3808ca4ac498c48797dedd95d647bc878c9
Cintia Osorio Duarte <cintia.o.duarte@gmail.com>
2023-10-26 21:46:05 -0300

    CO Agrego import android.widget.TextView a Confirmacion Final

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/actividades/ConfirmacionFinal.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/actividades/ConfirmacionFinal.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
-
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
