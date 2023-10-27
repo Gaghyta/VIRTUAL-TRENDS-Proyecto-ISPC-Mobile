4cc19bfbe96f747d794e377152136f15b10f59a5
AishaAmaya <Aishaamaya94@gmail.com>
2023-10-26 22:25:57 -0300

    codigo nuevo

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/actividades/ComprobacionReserva.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/actividades/ComprobacionReserva.java
@@ -24,6 +24,9 @@ public class ComprobacionReserva extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comprobacion_rerserva);
+        etComprobante = findViewById(R.id.et_turno_comprobante);
+        //intent
+        Intent i = getIntent();
         BottomNavigationView nav = findViewById(R.id.btnNavSelector);
         nav.setSelected(true);
 
