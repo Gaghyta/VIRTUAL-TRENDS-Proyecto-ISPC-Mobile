92cb99d7c000f8a41b71b700a9f4ab76d30d14ce
Cintia Osorio Duarte <cintia.o.duarte@gmail.com>
2023-10-26 21:49:24 -0300

    CO Agrego TextView tv;

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/actividades/ConfirmacionFinal.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/actividades/ConfirmacionFinal.java
@@ -14,6 +14,7 @@ import com.google.android.material.navigation.NavigationBarView;
 
 public class ConfirmacionFinal extends AppCompatActivity {
     String fecha, horario;
+    TextView tv;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmacion_final);
