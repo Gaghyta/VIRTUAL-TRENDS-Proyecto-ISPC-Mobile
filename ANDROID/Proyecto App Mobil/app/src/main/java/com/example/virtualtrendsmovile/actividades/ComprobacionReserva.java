25c3d9a7974ab778aa221c83331d2d55a59d5d52
Agostina <ago.95fiore@hotmail.com>
2023-10-28 18:02:59 -0300

    AF creo rama definitiva con codigo y diseño final de la app

--- ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/actividades/ComprobacionReserva.java
+++ ANDROID/Proyecto App Mobil/app/src/main/java/com/example/virtualtrendsmovile/actividades/ComprobacionReserva.java
@@ -24,7 +24,7 @@ public class ComprobacionReserva extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_comprobacion_rerserva);
+        setContentView(R.layout.activity_comprobacion_reserva);
         etComprobante = findViewById(R.id.et_turno_comprobante);
         //intent
         Intent i = getIntent();
